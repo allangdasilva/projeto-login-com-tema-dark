10ee8d13c4af299b7e71984d6ce80228d1c99361
Allan Silva <allandasilva33@outlook.com>
2024-07-14 03:29:33 -0300

    Adicionando a troca de imagens pelo javascript

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ theme.addEventListener('click', transition)
 
 function transition() {
     main.classList.toggle('active')
+
+    let img = document.querySelector('.left-login img')
+    if (main.classList.contains('active')) {
+        img.setAttribute('src', 'animação/cat-astronaut-animate (2).svg')
+    } else {
+        img.setAttribute('src', 'animação/cat-astronaut.svg')
+    }
 }
